2d73cda31f5135b10b8b4590383e5b964715b249
Owen Matteson <omatt431@mit.edu>
2024-11-21 13:18:57 -0500

    Remove utils.py (sigmoid func)

--- traj_opt/models/hopping_softfly/utils.py
+++
@@ -1,7 +0,0 @@
-import casadi as ca
-
-# Define sigmoid function for use in spring deformation constraints
-x = ca.MX.sym("x")
-k = ca.MX.sym("k") # Sigmoid steepness
-sigmoid_value = 1 / (1 + ca.exp(-k*x))
-sigmoid = ca.Function("sigmoid", [x, k], [sigmoid_value])
