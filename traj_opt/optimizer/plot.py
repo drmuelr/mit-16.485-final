0efebee34a62c00f9b0646aa60a0f286a310a724
Owen Matteson <omatt431@mit.edu>
2024-12-05 11:39:41 -0500

    Fix missing args in plotting script

--- traj_opt/optimizer/plot.py
+++ traj_opt/optimizer/plot.py
@@ -58,9 +58,9 @@ def animate_solution(optimizer, solution):
         # Z = optimizer.terrain_model.plot_func(X, Y)
         ax.plot_surface(X, Y, Z, color="gray", alpha=1.0)
     elif optimizer.terrain_type == "mesh":
-        optimizer.terrain_model.plot_surface()
+        optimizer.terrain_model.plot_surface(ax)
     elif optimizer.terrain_type == "sdf":
-        optimizer.terrain_model.plot_zero_crossing()
+        optimizer.terrain_model.plot_zero_crossing(ax)
 
     ax.set_xlim(xmin, xmax)
     ax.set_ylim(ymin, ymax)
